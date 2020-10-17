ccd8ad723770c245738055b3686b5690e23ab0a7
Felipe Gutierrez <1560@holbertonschool.com>
2020-10-17 17:05:00 -0500

    Files and directories

---
+++ static/js/script.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth2').removeClass('cS-hidden');
+        } 
+    });  
+  });
