a9bbe064ddc56c93fe5f62a53f051aacbb970188
ffelipegupe <1560@holbertonschool.com>
2020-10-20 00:40:01 +0000

    Adding files

---
+++ api/v1/views/index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+from models.store import Store
+from models import storage
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_objects():
+    """ Retrieves the number of each objects by type """
+    classes = [Store]
+    names = ["stores"]
+
+    num_objs = {}
+    for i in range(len(classes)):
+        num_objs[names[i]] = storage.count(classes[i])
+
+    return jsonify(num_objs)
