34a30679b42e5725fde7335375f109cb0f951929
daniloromero <danilo@LAPTOP-GDLDIUAI.localdomain>
2020-10-22 19:16:10 -0500

    removed commented mysql

--- dynamic/start.py
+++ dynamic/start.py
@@ -5,17 +5,10 @@ from flask import abort, jsonify, make_response, request
 from models.store import Store
 from models.food import Food
 from api.v1.views import app_views
-#from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-# MySQL connection
-#app.config['FASTQ_MYSQL_HOST'] = 'localhost'
-#app.config['FASTQ_MYSQL_USER'] = 'fastq_user'
-#app.config['FASTQ_MYSQL_PWD'] = 'fastq_dev_pwd'
-#app.config['FASTQ_MYSQL_DB'] = 'fastq_dev_db'
-#mysql = MySQL(app)
 
 @app.teardown_appcontext
 def close_db(error):
