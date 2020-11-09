04145560612c56ec12cffcedf0806466f56e116b
danielj32 <danieljuanito21@hotmail.com>
2020-11-09 00:42:57 -0500

    fix add button and this working dynamic

---
+++ dynamic/static/js/minplus2.js
@@ -0,0 +1,17 @@
+var total;
+// if user changes value in field
+$('.field').change(function() {
+  // maybe update the total here?
+}).trigger('change');
+
+$('.add').click(function() {
+  var target = $('.field', this.parentNode)[0];
+  target.value = +target.value + 1;
+});
+
+$('.sub').click(function() {
+  var target = $('.field', this.parentNode)[0];
+  if (target.value > 0) {
+    target.value = +target.value - 1;
+  }
+});
